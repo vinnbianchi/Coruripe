58382c0ae23d2d18684cccccb8f6914cc9f12d4d
Vinicius Gomes Bianchi <vinicius.bianchi@usinacoruripe.com.br>
2026-06-25 11:45:43 -0300

    Atualizacao automatica apos rodar script Python

--- rodar_projeto.bat
+++ rodar_projeto.bat
@@ -1,6 +1,9 @@
 @echo off
-:: 1. Entra na pasta do projeto
-cd /d "C:\Users\vgbianchi\OneDrive - SA USINA CORURIPE AÇUCAR E ALCOOL\Área de Trabalho\siteclima"
+:: Força o terminal a reconhecer caracteres com acento (como o Ú de AÇÚCAR)
+chcp 65001 > nul
+
+:: 1. Entra na pasta correta do projeto
+cd /d "C:\Users\vgbianchi\OneDrive - SA USINA CORURIPE AÇÚCAR E ALCOOL\Área de Trabalho\siteclima"
 
 :: 2. Executa a sua automação em Python
 python automacao_completa.py
@@ -9,4 +12,5 @@ python automacao_completa.py
 git add .
 git commit -m "Atualizacao automatica apos rodar script Python"
 git push origin main
+
 pause
