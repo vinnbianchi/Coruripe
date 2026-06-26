529289af05f671611161037c67a724a0775ffb9c
Vinicius <vinnbianchi@gmail.com>
2026-06-26 07:50:45 -0300

    teste

--- rodar_projeto.bat
+++ rodar_projeto.bat
@@ -1,16 +1,12 @@
 @echo off
-:: Força o terminal a reconhecer caracteres com acento (como o Ú de AÇÚCAR)
-chcp 65001 > nul
 
 :: 1. Entra na pasta correta do projeto
-cd /d "C:\Users\vgbianchi\OneDrive - SA USINA CORURIPE AÇÚCAR E ALCOOL\Área de Trabalho\siteclima"
+cd /d "C:\siteclima"
 
 :: 2. Executa a sua automação em Python
 python automacao_completa.py
 
-:: 3. Executa os comandos do Git para atualizar o GitHub
-git add .
-git commit -m "Atualizacao automatica apos rodar script Python"
-git push origin main
-
-pause
+:: 3. Executa os comandos do Git enviando APENAS o index.html
+git add index.html
+git commit -m "Atualizacao automatica do index.html"
+git push origin main --force
